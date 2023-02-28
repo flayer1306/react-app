9dd5e5b2b4af34b1b252932a1ad96e19733e98e6
Aleksandr Punyakin <electrodyn_ap@mail.ru>
2023-03-01 00:10:07 +0300

    add bootstrap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import reactDom from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css'
 
 const element = <h1>Hello World!</h1>;
 reactDom.render(element, document.querySelector('#root'));

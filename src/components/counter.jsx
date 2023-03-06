4db4b6450e7e88e9345f6497258eaf60e95760b3
flayer1306 <117988470+flayer1306@users.noreply.github.com>
2023-03-06 22:48:44 +0300

    add function js

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+const count = 2;
+const formatCount = () => {
+    return count === 0 ? 'empty' : count;
+}
 const Counter = () => {
-    return <h1>Counter!</h1>
+    return (
+        <>
+        <h1>{formatCount()}</h1>
+        </>
+    );
 }
 
 export default Counter;
--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import 'bootstrap/dist/css/bootstrap.css'
 import Counter from './components/counter';
 
 
-
 const App = () => {
-    return <Counter />;
+    return (
+        <>
+            <Counter/>
+            <button> + </button>
+        </>
+    );
 }
-reactDom.render(<App />, document.querySelector('#root'));
+reactDom.render(<App/>, document.querySelector('#root'));

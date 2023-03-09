0f2e03092b2ead269741aa70ec2f9e46c839b8cd
flayer1306 <117988470+flayer1306@users.noreply.github.com>
2023-03-09 21:40:09 +0300

    add dynamic styles and onClick

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 
-const count = 2;
+let count = 0;
 const formatCount = () => {
     return count === 0 ? 'empty' : count;
+}
+
+const getBadgeClasses = () => {
+    let classes = 'badge m-2 ';
+    classes += count === 0 ? 'bg-warning' : 'bg-primary';
+    return classes
+};
+
+const handleIncrement = () => {
+    count += 1;
+    console.log(count)
 }
 const Counter = () => {
     return (
         <>
-        <span>{formatCount()}</span>
+            <span className = {getBadgeClasses()}>{formatCount()}</span>
+            <button className = 'btn btn-primary btn-sm m-2' onClick={handleIncrement}>+</button>
         </>
     );
 }

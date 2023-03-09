24226caef7fb2acd36887c65e5d8e76ab00f1a33
flayer1306 <117988470+flayer1306@users.noreply.github.com>
2023-03-09 22:22:48 +0300

    add useState and decrement

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-let count = 0;
-const formatCount = () => {
-    return count === 0 ? 'empty' : count;
-}
 
-const getBadgeClasses = () => {
-    let classes = 'badge m-2 ';
-    classes += count === 0 ? 'bg-warning' : 'bg-primary';
-    return classes
-};
-
-const handleIncrement = () => {
-    count += 1;
-    console.log(count)
-}
 const Counter = () => {
+    const [count, setCount] = useState(0)
+    const formatCount = () => {
+        return count === 0 ? 'empty' : count;
+    }
+
+    const getBadgeClasses = () => {
+        let classes = 'badge m-2 ';
+        classes += count === 0 ? 'bg-warning' : 'bg-primary';
+        return classes
+    };
+
+    const handleIncrement = () => {
+        setCount((prevState) => prevState + 1);
+    };
+    const handleDecrement = () => {
+        setCount((prevState) => prevState - 1)
+    };
+
     return (
         <>
             <span className = {getBadgeClasses()}>{formatCount()}</span>
-            <button className = 'btn btn-primary btn-sm m-2' onClick={handleIncrement}>+</button>
+            <button className = 'btn btn-primary btn-sm m-2'
+                    onClick={handleIncrement}>
+                +
+            </button>
+            <button className = 'btn btn-primary btn-sm m-2'
+                    onClick={handleDecrement}>
+                -
+            </button>
         </>
     );
 }

a3c2625ab89bb3bc8a7399ec6c6b1b9a459f59a8
flayer1306 <117988470+flayer1306@users.noreply.github.com>
2023-03-11 00:23:18 +0300

    add render array

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -20,9 +20,30 @@ const Counter = () => {
         setCount((prevState) => prevState - 1)
     };
 
+    const [ tags, setTags ] = useState([ 'tag1', 'tag2', 'tag3' ]);
+    const handleChangeTag = (id) => {
+        setTags(prevState => prevState.filter(item => item !== id));
+    };
+
+    const renderTags = () => {
+        return tags.length !== 0 ?
+            tags.map((tag) => (
+                <li key={tag} className='btn btn-primary btn-sm m-2'
+                    onClick={() => handleChangeTag(tag)}>
+                    {tag}
+                </li>
+            )) : 'No tags';
+    }
+
+
     return (
         <>
-            <span className={getBadgeClasses()}>{formatCount()}</span>
+            <ul>
+                {renderTags()}
+            </ul>
+            <span className={getBadgeClasses()}>
+                {formatCount()}
+            </span>
             <button className='btn btn-primary btn-sm m-2'
                     onClick={handleIncrement}>
                 +

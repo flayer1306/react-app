8526ca00a70122d4671846dd226405bd30ac4881
flayer1306 <117988470+flayer1306@users.noreply.github.com>
2023-03-18 22:43:31 +0300

    reformat code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import reactDom from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
-import Counter from './components/counter';
+import App from './app';
 
 
-const App = () => {
-    return (
-        <>
-            <Counter/>
-        </>
-    );
-}
+
 reactDom.render(<App/>, document.querySelector('#root'));
